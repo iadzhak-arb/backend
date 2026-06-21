4b36f9b50f7c0a0a53fec935162cbaacf8ca4943
Iadzhak Sergei <yadzhak@yandex.ru>
2026-06-21 12:58:48 +0300

    api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_swagger.views import get_swagger_view
 
 from .views import ArbitrageViewSet, MarketViewSet
 
@@ -10,5 +9,4 @@ router.register('markets', MarketViewSet, 'markets')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('docs/', )
 ]
